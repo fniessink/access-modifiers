56ec1fd3473372d5db1808a41b51658080b9fb34
Frank Niessink <frank@niessink.com>
2019-08-24 11:22:58 +0200

    Add Travis build.

--- setup.py
+++ setup.py
@@ -18,5 +18,9 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.8"
         "Topic :: Software Development"])
