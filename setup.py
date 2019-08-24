1c3800e09d6d283ba08e257c95bcb4bbaf32e18b
Frank Niessink <frank@niessink.com>
2019-08-24 20:22:45 +0200

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="access_modifiers",
-    version="0.1.2",
+    version="0.1.3",
     author="Frank Niessink",
     author_email="frank@niessink.com",
     description="Private and protected access modifiers for Python",
