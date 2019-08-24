978924b86ffb494a3494d39c8175b9151a7902fb
Frank Niessink <frank@niessink.com>
2019-08-24 15:35:45 +0200

    Use bump2version for releases.

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+
+<!-- markdownlint-disable MD024 -->
+
+All notable changes to *access-modifiers* will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+<!-- The line "## <square-bracket>Unreleased</square-bracket>" is replaced by the ci/release.py script with the new release version and release date. -->
+
+## [Unreleased]
+
+### Fixed
+
+- Added PyPI installation instructions.
+
+## [0.1.0] - [2019-08-24]
+
+### Added
+
+- Initial release. 
---
+++ ci/release.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""Release the package."""
+
+import datetime
+import argparse
+import os
+import subprocess
+
+
+def parse_arguments():
+    """Return the command line arguments."""
+    bump = subprocess.check_output(("bump2version", "--list", "--dry-run", "--allow-dirty", "part"), text=True)
+    current_version = [line for line in bump.split("\n") if line.startswith("current_version")][0].split("=")[1]
+    parser = argparse.ArgumentParser(description=f'Release access-modifiers. Current version is {current_version}.')
+    parser.add_argument('version', choices=('patch', 'minor', 'major'))
+    return parser.parse_args()
+
+
+def main():
+    """Create the release."""
+    os.environ["RELEASE_DATE"] = datetime.date.today().isoformat()  # Used by bump2version to update CHANGELOG.md
+    subprocess.run(("bump2version", parse_arguments().version), check=True)
+    subprocess.run(("git", "push", "--follow-tags"), check=True)
+
+
+if __name__ == "__main__":
+    main()
